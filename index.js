4cd986a5526fb93725f8e2d77c3c3454dccd9157
Aditya <adityathebest2019@gmail.com>
2024-02-16 11:11:47 +0530

    axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
@@ -7,12 +8,25 @@ app.get("/test", (req, res) => {
     res.send("Working!");
 });
 
-const cronjob = () =>
-    fetch("https://ncsify.onrender.com/cron")
-        .then((res) => res.text())
-        .then((data) => {
-            console.log(data);
+// const cronjob = () =>
+//     ("https://ncsify.onrender.com/cron")
+//         .then((res) => res.text())
+//         .then((data) => {
+//             console.log(data);
+//         })
+//         .catch((error) => {
+//             console.error("Error fetching URL:", error);
+//         });
+const cronjob = async () => {
+    axios
+        .get("https://ncsify.onrender.com/cron")
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error("Error fetching URL:", error);
         });
+};
 
 const interval = 1000 * 60 * 5;
 
