fa156a3add1e713870ca50ebf7e060b786cc2a7a
Aditya <adityathebest2019@gmail.com>
2024-02-16 07:37:35 +0530

    sidebranch cron test

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-fetch("https://ncsify.onrender.com/cron")
-    .then((res) => res.text())
-    .then((data) => {
-        console.log(data);
-    });
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const cronjob = () =>
+    fetch("https://ncsify.onrender.com/cron")
+        .then((res) => res.text())
+        .then((data) => {
+            console.log(data);
+        });
+
+const interval = 1000 * 60 * 14;
+
+setInterval(cronjob, interval);
+app.listen(process.env.PORT || 8000);
